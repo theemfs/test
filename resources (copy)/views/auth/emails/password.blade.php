0a73157fbfa30160c68fa841fe948cdff1cc133f
theemfs <theemfs@gmail.com>
2016-01-06 01:51:35 +0800

    remove trash

--- resources (copy)/views/auth/emails/password.blade.php
+++
@@ -1,5 +0,0 @@
-Click here to reset your password:
-
-{{ url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}
-
-:-)
--- resources (copy)/views/tasks/show.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-	<h2>Task #{{ $task->id }}</h2>
-	<pre>Task {{ $task->task_text}}</pre>
-@endsection
